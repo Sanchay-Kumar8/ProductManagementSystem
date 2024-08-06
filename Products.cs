bf4536f25261735fbee254d886ec9bb9ccf40af8
Satyam Nema <satyam.nema@capgemini.com>
2024-08-06 13:56:22 +0530

    Product Updated

--- Products.cs
+++ Products.cs
@@ -8,6 +8,32 @@ namespace productManagementSystem1
 {
     internal class Products
     {
+        public int prodId { get; set; }
+        public string prodName { get; set; }
+        public int prodPrice { get; set; }
+        public string prodDescription { get; set; }
+
+        public void AddProduct()
+        {
+            Console.WriteLine("Product added successfully");
+        }
+
+        public void RemoveProduct()
+        {
+            Console.WriteLine("Product deleted successfully");
+        }
+
+        public int GetProductById(int id)
+        {
+            id = 0;
+            Console.WriteLine("Product fetched successfully");
+            return id;
+        }
+
+        public void UpdateProduct(int id)
+        {
+            Console.WriteLine("Product updated successfully");
+        }
 
     }
 }
