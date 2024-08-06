c9cec5832b6237729bf608ec64d8f285b6c80d1c
Sanchay Kumar <sanchay.kumar@capgemini.com>
2024-08-06 13:06:03 +0530

    commit done by Sanchay

--- Shipment.cs
+++ Shipment.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace productManagementSystem1
 {
-    internal class Shipment
+    public class Shipment
     {
+        public int OrderID { get; set; }
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string Address { get; set; }
+        public int ShipmentCost { get; set; }
+        public DateTime DateOfPurchase { get; set; }
+        public DateTime ExpectedDateOfDelivery { get; set; }
+
+
+        public int GetShipmentID()
+        {
+            return OrderID;
+        }
+
+        public int GetShipmentCost()
+        {
+            return ShipmentCost;
+        }
+
     }
 }
