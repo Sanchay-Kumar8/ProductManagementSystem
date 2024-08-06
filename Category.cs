8d2e3e5d134e9daa1ce148e7154c7806887b3aa0
shah-zx <shahnawazsayyed456@gmail.com>
2024-08-06 13:03:41 +0530

    Added.

--- Category.cs
+++ Category.cs
@@ -8,5 +8,22 @@ namespace productManagementSystem1
 {
     internal class Category
     {
+
+        public int CatId { get; set; }
+        public string CatName { get; set; }    
+
+        public string Description { get; set; } 
+
+        public void TagCategory()  // Will tag the category with the products 
+        {
+            Console.WriteLine("Product successfully tagged with provided category !!");
+        }
+
+        public int CountCategory(string product_name)  // Will provide the count of the the products present in the category //
+        {
+            return 1;
+        }
+        
+
     }
 }
